6fe4e3ff8047383176bed4abe245f36d4e800fb5
czylabsonasa <czylabson.asa@gmail.com>
2026-03-02 11:24:15 +0100

    up

---
+++ store/leastsquares/peter_acc_solution.m
@@ -0,0 +1,52 @@
+% Peter - accel - solution
+clc; clf; clear;
+hold on;
+
+rng(2*22*22);
+
+% transform the
+t=0:2:12;
+acc=2.22;
+f=acc/2*t.^2+t.*datasample(-10:10,length(t))/12;
+f=round(f,2);
+f(1)=0;
+
+f
+
+% f
+% return
+
+% we are also examine the rref
+% transforming them to a column vector
+t=t(:); f=f(:);
+% build the gaussian equation
+A=[ones(size(t)), t.^2];
+fprintf("- the rref of the gaussian normal equation:\n\n")
+disp(rref([A'*A, A'*f]))
+fprintf("\n");
+
+% also using the backslash to solve the system
+x=(A'*A)\(A'*f);
+
+% we are searching the in s+a/2*t^2 form
+s=x(1); 
+a=2*x(2);
+
+plot(t,f,"*","MarkerSize",8);
+% we need more points to plot a nice parabola
+xx=linspace(-1,13);
+plot(xx,s+a/2*xx.^2,"r","LineWidth",1);
+
+legend(["data points","the best fitting function"],"Location","southeast");
+axis([-1,13,-inf,inf]);
+
+saveas(gca(),"peter_acc_solution.png","png");
+
+fprintf("- the best function is: %.4f + %.4f/2*t^2\n",s,a);
+fprintf("- at t=13 the position will be x=%.4f + %.4f/2*13^2 = %.4f\n",...
+   s,a,s+a/2*13^2);
+fprintf("- the position x=1000 will be reached at t=%.4f\n",sqrt(2*(1000-s)/a));
+fprintf("- the acceleration is a=%.4f\n",a);
+
+
+hold off;
---
+++ store/leastsquares/peter_water_solution.m
@@ -0,0 +1,41 @@
+% Peter - watertank - solution
+clc; clf; clear;
+hold on;
+
+% transform the
+t=0:2:12;
+f=[3,4,5,5.5,6.5,7,8];
+
+% we are also examine the rref
+% transforming them to a column vector
+t=t(:); f=f(:);
+% build the gaussian equation
+A=[ones(size(t)), t];
+fprintf("- the rref of the gaussian normal equation:\n\n")
+disp(rref([A'*A, A'*f]))
+fprintf("\n");
+
+
+
+p=polyfit(t,f,1);
+% we are searching the line in b+at form
+a=p(1); b=p(2);
+
+plot(t,f,"*","MarkerSize",8);
+% saveas(gca(),"peter_water_data.png","png");
+% return;
+
+plot([-1,13],polyval(p,[-1,13]),"r","LineWidth",1);
+
+legend(["data points","the best fitting line"],"Location","southeast");
+axis([-1,13,2,9]);
+
+saveas(gca(),"peter_water_solution.png","png");
+
+fprintf("- the best line is: %.4f + %.4f*t\n",b,a);
+fprintf("- at t=20 the height of the water is: %.4f + %.4f*20 = %.4f\n",b,a,polyval(p,20));
+fprintf("- the tank will be full at t=(50-%.4f)/%.4f = %.4f\n",b,a,(50-b)/a);
+fprintf("- the tank was empty at t=(0-%.4f)/%.4f = %.4f\n",b,a,(0-b)/a);
+
+
+hold off;
